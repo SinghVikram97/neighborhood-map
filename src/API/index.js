512fe2035ec98846294498d8d6067bbcbbe687bb
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2018-12-13 12:57:04 +0530

    Map loads with a single marker

---
+++ src/API/index.js
@@ -0,0 +1,49 @@
+class Helper{
+    static baseURL(){
+        return "https://api.foursquare.com/v2";
+    }
+    // client_id=blah&client_secret=blah&v=blah
+    static auth(){
+        const keys={
+            client_id:'UKEAXMUNOMTJMELYEET5IGZ0HTEIARIBDFDJ2R4OZAFXZRH4',
+            client_secret:'JERZPA51SVRMTOM3CYJGLZDQTUK5LXPIGR4LUELOY3QJAKCV',
+            v:"20181213"
+        };
+        return Object.keys(keys)
+            .map(key=>`${key}=${keys[key]}`)
+            .join('&')
+    }
+    static urlBuilder(urlParams){
+        if(!urlParams){
+            return ""
+        }
+        return Object.keys(urlParams)
+            .map(key=>`${key}=${urlParams[key]}`)
+            .join('&');
+    }
+    static headers(){
+        return{
+            Accept:"application/json"
+        }
+    }
+    static simpleFetch(endPoint,method,urlParams){
+        let requestData={
+            method,
+            headers:Helper.headers()
+        };
+        return fetch(`${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(urlParams)}`,
+               requestData
+        ).then(res=>res.json())
+    }
+}
+export default class  SquareAPI{
+    static search(urlParams){
+        return Helper.simpleFetch("/venues/search","GET",urlParams)
+    }
+    static venueDetails(venue_id){
+        return Helper.simpleFetch(`/venues/${venue_id}`,"GET");
+    }
+    static venuePhotos(venue_id){
+        return Helper.simpleFetch(`/venues/${venue_id}/photos`,"GET");
+    }
+}
